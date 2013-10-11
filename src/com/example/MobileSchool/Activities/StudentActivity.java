30e275d6feeaac6557c2df8999feb19e72f6e8f7
Kimyuhwan <yuhwan@nclab.kaist.ac.kr>
2013-10-11 20:36:21 +0900

    Add Basic Teacher and Student Activities

---
+++ src/com/example/MobileSchool/Activities/StudentActivity.java
@@ -0,0 +1,9 @@
+package com.example.MobileSchool.Activities;
+
+/**
+ * User: yuhwan
+ * Date: 13. 10. 11
+ * Time: 오후 8:30
+ */
+public class StudentActivity {
+}
---
+++ src/com/example/MobileSchool/Activities/TeacherRoomActivity.java
@@ -0,0 +1,9 @@
+package com.example.MobileSchool.Activities;
+
+/**
+ * User: yuhwan
+ * Date: 13. 10. 11
+ * Time: 오후 8:32
+ */
+public class TeacherRoomActivity {
+}
